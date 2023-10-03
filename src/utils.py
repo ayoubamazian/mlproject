5d3052be783646a6f59297a0c69e4b1ac4b953d5
Admin <Admin@PC-ADMIN>
2023-10-03 13:31:44 +0100

    Predict Pipeline and inteface with flask done

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+import pickle
 
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
@@ -45,3 +46,10 @@ def evaluate_models(x_train, y_train, x_test, y_test, models, parms):
 
     except Exception as e:
         raise CustomException(e,sys)
+    
+def load_object(path):
+    try:
+        with open(path, 'rb') as file:
+            return dill.load(file)
+    except Exception as e:
+        raise CustomException(e, sys)
